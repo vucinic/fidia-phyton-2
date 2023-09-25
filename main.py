2eb5f3f6be252026a16dec8cb746f8da5ae63699
avtk <a.viticchie@awaves.it>
2023-09-25 11:59:59 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+class Data:
+
+    def __init__(self,
+                 g: int | str = None,
+                 m: int = None,
+                 a: int = None):
+        self.g = g if g is not None else 0
+        self.__m = m if m is not None else 0
+        self.__a = a if a is not None else 0
+
+
+
+with open("./input.csv", 'r') as c:
+    c.read()
+
+
+    fsd
+
+
+
