76f6bbbf480ee0451460d242a8093521290e887a
avtk <a.viticchie@awaves.it>
2023-11-20 11:44:36 +0100

    first step exe

--- 05_esercizio/stats.py
+++ 05_esercizio/stats.py
@@ -1,13 +1,19 @@
+import random
+import sys
+
 import psutil
 
 
 def get_cpu_temperature():
     # for g in psutil.sensors_temperatures(False)['coretemp']:
     #     print(g)
-    cpu = [i[1] for i in psutil.sensors_temperatures(False)['coretemp'] if 'Core' in i[0]]
-    cpu_temp = sum(cpu) / len(cpu)
-    # print(cpu_temp)
-    return cpu_temp
+    if sys.platform == 'win32':
+        return random.Random().random() * 100
+    else:
+        cpu = [i[1] for i in psutil.sensors_temperatures(False)['coretemp'] if 'Core' in i[0]]
+        cpu_temp = sum(cpu) / len(cpu)
+        # print(cpu_temp)
+        return cpu_temp
 
 
 def get_cpu_percent():
